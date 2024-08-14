3679a72f581b1709c65b030040e07524f4fff03f
aman1117 <arl0817osho@gmail.com>
2024-08-14 05:10:43 +0000

    connected database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
+import { EmployeesModule } from './employees/employees.module';
 
 
 // root module
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, DatabaseModule, EmployeesModule],
   controllers: [AppController],
   providers: [AppService],
 })
